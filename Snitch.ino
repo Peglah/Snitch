3cc4e30785c3b726a95bc5ca082aebcbd0414159
Peglah <peglah@gmail.com>
2018-11-28 15:48:52 +0100

    Update Snitch.ino

--- Snitch.ino
+++ Snitch.ino
@@ -1,5 +1,4 @@
 /*********
-  Complete project details at https://randomnerdtutorials.com
   Based on ESP32_NTPClient by Rui Santos
   https://github.com/RuiSantosdotme/Random-Nerd-Tutorials/blob/master/Projects/ESP32/ESP32_NTPClient.ino
 *********/
@@ -15,16 +14,23 @@ const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "se.pool.ntp.org", 3600, 60000);
 
 // Variables to save date and time
 String formattedDate;
 String dayStamp;
 String timeStamp;
+int hours;
+int minutes;
 
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
+  //while(!Serial);
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -32,26 +38,22 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  
   // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-// Initialize a NTPClient to get time
+  // Initialize a NTPClient to get time
   timeClient.begin();
-  // NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
-  timeClient.setTimeOffset(3600);
 }
+
 void loop() {
   while(!timeClient.update()) {
     timeClient.forceUpdate();
   }
+  
   // The formattedDate comes with the following format:
   // 2018-05-28T16:00:13Z
   // We need to extract date and time
@@ -67,5 +69,10 @@ void loop() {
   timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
   Serial.print("HOUR: ");
   Serial.println(timeStamp);
+  // Split hours and minutes
+  hours = (timeStamp.substring(0, 2)).toInt();
+  minutes = (timeStamp.substring(2, 4)).toInt();
   delay(1000);
+  
+  
 }
