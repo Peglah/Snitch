9b4eda288520ffc3505ed4839a78faf5d8a479c6
Peglah <peglah@gmail.com>
2023-07-19 23:04:52 +0200

    changed everything

--- src/main.cpp
+++ src/main.cpp
@@ -3,249 +3,97 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <FastLED.h>
-#include <ArduinoJson.h>
 
 #define projectName "ESP32-Snitch"
 
 #define NUM_LEDS 16
 #define DATA_PIN 12
-CRGB leds[NUM_LEDS];
-
-// Timer for when to update NTP
-unsigned long timePreviousMillis = 0;               // will store last time
-const unsigned long timeInterval = 10 * 60 * 1000;  // interval at which to run (milliseconds)
-
-// Timer for when to update temperature
-unsigned long tempPreviousMillis = 0;               // will store last time
-const unsigned long tempInterval = 5 * 60 * 1000;  // interval at which to run (milliseconds)
-
-// Timer for when to switch between time and temperature
-unsigned long displayPreviousMillis = 0;               // will store last time
-const unsigned long displayInterval = 60 * 1000;  // interval at which to run (milliseconds)
-
-String CityID = "REPLACE_WITH_YOUR_CITY";  //Jursla, SE
-String APIKEY = "REPLACE_WITH_YOUR_API_KEY";
-
-const bool childClock = true; // Override binary time and temperature. Show child clock instead
 
 // Time in seconds
 const int AwakeHour = 7 * 3600; // all Green
-const int SleepHour = 18 * 3600; // all Red
+const int SleepHour = 19 * 3600; // all Red
 const int PreAwakeHour = AwakeHour - (1 * 3600); // Red to Green
+const int PreSleepHour = SleepHour - (1 * 3600); // Green to Red
 
-WiFiClient client; // Used to get temperature
-const char* servername = "api.openweathermap.org"; // remote server we will connect to
+const int brightness = 1; // Set the brightness level
+
+int red[3] = {brightness, 0, 0};
+int green[3] = {0, brightness, 0};
+
+CRGB leds[NUM_LEDS];
 
 WiFiUDP ntpUDP; // Used by NTPClient to get time
-
-// You can specify the time server pool and the offset (in seconds, can be
-// changed later with setTimeOffset() ). Additionaly you can specify the
-// update interval (in milliseconds, can be changed using setUpdateInterval() ).
 NTPClient timeClient(ntpUDP, "se.pool.ntp.org", 7200, 60000);
 
-bool doTime = true;
-int temperature = -273;
+unsigned long lastNTPUpdate = 0; // Stores the last time the NTP client was updated
+const unsigned long NTPUpdateInterval = 3600000; // Update interval for NTP client in milliseconds (1 hour)
 
-float computeHeatIndex(float temperature, int percentHumidity) {
-  temperature = temperature * 1.8 + 32;
-  float hi = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (percentHumidity * 0.094));
+class DummyTimeClient {
+  private:
+    unsigned long currentTime = 0;
+    const unsigned long dayNightCycleDuration = 24 * 3600; // Duration of a day-night cycle in seconds
 
-  if (hi > 79) {
-    hi = -42.379 +
-         2.04901523 * temperature +
-         10.14333127 * percentHumidity +
-         -0.22475541 * temperature * percentHumidity +
-         -0.00683783 * pow(temperature, 2) +
-         -0.05481717 * pow(percentHumidity, 2) +
-         0.00122874 * pow(temperature, 2) * percentHumidity +
-         0.00085282 * temperature * pow(percentHumidity, 2) +
-         -0.00000199 * pow(temperature, 2) * pow(percentHumidity, 2);
+  public:
+    void begin() {
+      currentTime = 0 * 3600; // Initialize time to start of day-night cycle
+    }
 
-    if ((percentHumidity < 13) && (temperature >= 80.0) && (temperature <= 112.0))
-      hi -= ((13.0 - percentHumidity) * 0.25) * sqrt((17.0 - abs(temperature - 95.0)) * 0.05882);
-
-    else if ((percentHumidity > 85.0) && (temperature >= 80.0) && (temperature <= 87.0))
-      hi += ((percentHumidity - 85.0) * 0.1) * ((87.0 - temperature) * 0.2);
-  }
-  return (hi - 32) * 0.55555;
-}
-
-int getTemperature() {
-  String json;
-  int tempInt = -273;
-  float tempFloat = -273;
-  const int httpPort = 80;
-
-  const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + 2 * JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(12) + 270;
-  DynamicJsonDocument doc(capacity);
-
-  if (client.connect(servername, httpPort)) {
-    // We now create a URI for the request
-    String url = "/data/2.5/weather?id=" + CityID + "&units=metric&APPID=" + APIKEY;
-
-    // This will send the request to the server
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + servername + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-      if (millis() - timeout > 5000) {
-        client.stop();
-        return tempInt;
+    void update() {
+      currentTime++; // Increment current time by 1 second
+      if (currentTime >= dayNightCycleDuration) {
+        currentTime = 0; // Reset time to start of day-night cycle
       }
+      Serial.printf("Current time: %02d:%02d:%02d\n", getHours(), getMinutes(), getSeconds());
     }
 
-    // Read all the lines of the reply from server
-    while (client.available()) {
-      json = client.readStringUntil('\r');
+    int getHours() {
+      return (currentTime / 3600) % 24;
     }
 
-    // Parse the json and extract the temperature
-    deserializeJson(doc, json);
+    int getMinutes() {
+      return (currentTime / 60) % 60;
+    }
 
-    JsonObject main = doc["main"];
-    float main_temp = main["temp"];
-    int main_humidity = main["humidity"];
+    int getSeconds() {
+      return currentTime % 60;
+    }
+};
 
-    Serial.print("Main temp: ");
-    Serial.println(main_temp);
-    Serial.print("Main humidity: ");
-    Serial.println(main_humidity);
+//DummyTimeClient timeClient;
 
-    tempFloat = computeHeatIndex(main_temp, main_humidity);
-
-    Serial.print("Heat index: ");
-    Serial.println(tempFloat);
-
-    // Round and store with out decimals.
-    tempInt = round(tempFloat);
-
-    Serial.print("Rounded heat index: ");
-    Serial.println(tempInt);
-
-    return tempInt;
-  }
-
-  else {
-    Serial.println("Unable to connect");
-    return tempInt;
+void setLEDs(int color[3]) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].setRGB(color[0], color[1], color[2]);
   }
 }
 
-void drawTemperature(int temperature) {
-  if (temperature >= NUM_LEDS + 1) {
-    // Up and Red
-    for (int i = 0; i < NUM_LEDS; i++) {
-      if (temperature - 16 > i) {
-        leds[i].setRGB(1, 0, 0);
-      }
-      else {
-        leds[i] = CRGB::Black;
-      }
-    }
-  }
-  else if (temperature >= 1 && temperature <= NUM_LEDS) {
-    // Up and Green
-    for (int i = 0; i < NUM_LEDS; i++) {
-      if (temperature > i) {
-        leds[i].setRGB(0, 1, 0);
-      }
-      else {
-        leds[i] = CRGB::Black;
-      }
-    }
-  }
-  else if (temperature == 0) {
-    // All off
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Black;
-    }
-  }
-  else if (temperature <= -1 && temperature >= -16) {
-    // Down and Blue
-    for (int i = NUM_LEDS - 1; i >= 0; i--) {
-      if (temperature < 0) {
-        leds[i].setRGB(0, 0, 1);
-        temperature++;
-      }
-      else {
-        leds[i] = CRGB::Black;
-      }
-    }
-  }
-  else if (temperature == -273) {
-    for (int i = 0; i < 16; i = i + 2) {
-      leds[i].setRGB(0, 0, 1);
-      leds[i+1] = CRGB::Black;
+void transitionLEDs(int TimeDaySec, int startHour, int endHour, int startColor[3], int endColor[3]) {
+  // Calculate the transition period and threshold as floating-point numbers to allow for fractional values.
+  // This ensures a smooth transition even when the time or number of LEDs doesn't divide evenly.
+  float transitionPeriod = static_cast<float>(endHour - startHour) / NUM_LEDS;
+  float threshold = static_cast<float>(TimeDaySec - startHour) / transitionPeriod;
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (i < threshold) {
+      leds[i].setRGB(endColor[0], endColor[1], endColor[2]); // Swap start and end colors
+    } else {
+      leds[i].setRGB(startColor[0], startColor[1], startColor[2]); // Swap start and end colors
     }
   }
 }
 
 void drawChildClock(int TimeDaySec) {
   if (TimeDaySec > SleepHour || TimeDaySec < PreAwakeHour) {
-    // All Red
-    for (int i = 0; i  < NUM_LEDS; i++) {
-      leds[i].setRGB(1, 0, 0);
-    }
+    setLEDs(red); // All Red
   }
-  else if (TimeDaySec > AwakeHour) {
-    // All Green
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i].setRGB(0, 1, 0);
-    }
+  else if (TimeDaySec > AwakeHour && TimeDaySec <= PreSleepHour) {
+    setLEDs(green); // All Green
   }
-  else if (TimeDaySec > PreAwakeHour) {
-    // Red to Green
-
-    float threshold = static_cast<float>(TimeDaySec - PreAwakeHour) / (60.0 * 3.75);
-
-    for (int i = 0; i < NUM_LEDS; i++) {
-      if (i < threshold) {
-        leds[i].setRGB(0, 1, 0);
-      } else {
-        leds[i].setRGB(1, 0, 0);
-      }
-    }
+  else if (TimeDaySec > PreAwakeHour && TimeDaySec <= AwakeHour) {
+    transitionLEDs(TimeDaySec, PreAwakeHour, AwakeHour, red, green); // Red to Green
   }
-}
-
-void drawTime(String sHours, String sMinutes) {
-  while (5 - sHours.length() > 0) {
-    sHours = "0" + sHours;
-  }
-  while (8 - sHours.length() > 0) {
-    sHours = sHours + "-";
-  }
-
-  while (6 - sMinutes.length() > 0) {
-    sMinutes = "0" + sMinutes;
-  }
-  while (8 - sMinutes.length() > 0) {
-    sMinutes = "-" + sMinutes;
-  }
-
-  for (int i = 0; i < sHours.length(); i++) {
-    if (sHours[i] == '0') {
-      leds[i].setRGB(1, 0, 0);
-    }
-    if (sHours[i] == '1') {
-      leds[i].setRGB(0, 1, 0);
-    }
-    if (sHours[i] == '-') {
-      leds[i].setRGB(0, 0, 1);
-    }
-  }
-
-  for (int i = 0; i < sMinutes.length(); i++) {
-    if (sMinutes[i] == '0') {
-      leds[i+8].setRGB(1, 0, 0);
-    }
-    if (sMinutes[i] == '1') {
-      leds[i+8].setRGB(0, 1, 0);
-    }
-    if (sMinutes[i] == '-') {
-      leds[i+8].setRGB(0, 0, 1);
-    }
+  else if (TimeDaySec > PreSleepHour && TimeDaySec <= SleepHour) {
+    transitionLEDs(TimeDaySec, PreSleepHour, SleepHour, green, red); // Green to Red
   }
 }
 
@@ -295,9 +143,7 @@ void setup() {
   // Connect to Wifi
   connectWifi();
 
-  // Initialize a NTPClient and get time
   timeClient.begin();
-  timeClient.update();
 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
@@ -311,49 +157,16 @@ void setup() {
 }
 
 void loop() {
-  if (childClock) {
-    int TimeDaySec = timeClient.getHours() * 3600 + timeClient.getMinutes() * 60 + timeClient.getSeconds();
-    drawChildClock(TimeDaySec);
-  }
-  else if (doTime) { // Do the time thing
-    // Don't update from internet every second. Instead update every
-    // "interval" (default 10 mins).
-    if (millis() - timePreviousMillis >= timeInterval) {
-      while (!timeClient.update()) {
-        timeClient.forceUpdate();
-      }
-      timePreviousMillis = millis();
-    }
-    // The getHours and getMinutes comes with the following format:
-    // 16 and 00
-    // We need to use the String function to make it binary.
-    String sHours = String(timeClient.getHours(), BIN);
-    String sMinutes = String(timeClient.getMinutes(), BIN);
+  unsigned long currentMillis = millis(); // Get the current time in milliseconds
 
-    drawTime(sHours, sMinutes);
-  }
-  else { // Do the temp thing
-    if (millis() - tempPreviousMillis >= tempInterval) {
-      temperature = getTemperature();
-      Serial.print(temperature);
-      Serial.println(" degrees celsius.");
-      tempPreviousMillis = millis();
-    }
-    drawTemperature(temperature);
+  // Check if it's time to update the NTP client
+  if (currentMillis - lastNTPUpdate > NTPUpdateInterval) {
+    timeClient.update(); // Update the NTP client
+    lastNTPUpdate = currentMillis; // Save the time of the last update
   }
 
-  if (millis() - displayPreviousMillis >= displayInterval) {
-    doTime = !doTime;
-    displayPreviousMillis = millis();
-  }
-
-  /* TIME DEBUG
-  Serial.print(timeClient.getHours());
-  Serial.print(" ");
-  Serial.print(timeClient.getMinutes());
-  Serial.print(" ");
-  Serial.println(timeClient.getSeconds());
-  */
+  int TimeDaySec = timeClient.getHours() * 3600 + timeClient.getMinutes() * 60 + timeClient.getSeconds();
+  drawChildClock(TimeDaySec);
 
   FastLED.show();
   delay(1000);
